6ad25c005a278d205b2919a6f98ad921b128e05e
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-11-20 20:56:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-// Select all elements with the class "square"
-let boxs = document.querySelectorAll(".square");
+//your JS code here. If required.
+function focusSquare(){
+	let square1 = document.getElementById("square1");
+	let square2 = document.getElementById("square2");
+	let square3 = document.getElementById("square3");
 
-boxs.forEach((box, index) => {
-  box.addEventListener('mouseover', () => {
-    // Change the color of other squares to Coffee
-    boxs.forEach((s, i) => {
-      if (i !== index) {
-        s.style.backgroundColor = '#6F4E37'; // Coffee
-      }
-    });
-  });
+	function resetColors(){
+		square1.style.backgroundColor = "#E6E6FA";
+		square2.style.backgroundColor = "#E6E6FA";
+		square3.style.backgroundColor = "#E6E6FA";
+	}
 
-  box.addEventListener('mouseleave', () => {
-    // Reset all squares to Lavender
-    boxs.forEach(s => {
-      s.style.backgroundColor = '#E6E6FA'; // Lavender
-    });
-  });
-});
+	square1.addEventListener("mouseover",function() {
+
+		square2.style.backgroundColor = "#6F4E37";
+		square3.style.backgroundColor = "#6F4E37";
+	});
+
+	square2.addEventListener("mouseover",function() {
+
+		square1.style.backgroundColor = "#6F4E37";
+		square3.style.backgroundColor = "#6F4E37";
+	});
+
+	square3.addEventListener("mouseover",function() {
+
+		square1.style.backgroundColor = "#6F4E37";
+		square2.style.backgroundColor = "#6F4E37";
+	});
+
+	[square1 , square2 , square3].forEach((square) =>{
+		square.addEventListener("mouseout" , resetColors);
+	});
+	
+}
+focusSquare();

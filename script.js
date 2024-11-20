8f02ecb4d313824d5ef73d979713c6937b9746ad
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-11-20 20:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let boxs = document.querySelectorAll(".square");
+
+boxs.forEach ((box, index) => {
+	box.addEventListener('mouseenter', () => {
+		boxs.forEach((s, i) => {
+			if (i !== index) {
+				s.style.backgroundColor = '#6F4E37';
+			}
+		});
+	})
+	box.addEventListener('mouseleave', () => {
+		boxs.forEach(s => {
+			s.style.backgroundColor = '#E6E6FA';
+		});
+	});
+});
